22ebdccd85b8f666014af77d9c947678e4fbedc0
Dan Marsden <dan@danmarsden.com>
2019-04-12 09:50:00 +1200

    Keep gdpr unit tests for 3.6 and higher happy.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -62,6 +62,14 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     }
 
     /**
+     * Get the list of users who have data within a context.
+     *
+     * @param   userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+    }
+
+    /**
      * Export all user data for the specified user, in the specified contexts.
      *
      * @param approved_contextlist $contextlist The approved contexts to export information for.
@@ -83,5 +91,13 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      * @param   approved_contextlist    $contextlist    The approved contexts and user information to delete information for.
      */
     public static function delete_data_for_user(approved_contextlist $contextlist) {
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param   approved_userlist       $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
     }
 }
